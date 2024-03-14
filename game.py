a1b308bee181f0f15dd469dc4b5715a1d28c3449
Josh Taylor <josh.taylor3141@gmail.com>
2024-03-14 14:31:00 -0500

    Preparing for refactor

--- game.py
+++ game.py
@@ -3,11 +3,11 @@ import pygame as p
 
 board_flip = False
 
-def new_game():
+def new_game(): # Initializes the board and statuses
     pass
 
-def game_over():
+def game_over(): # If game is over, allows a new game or review of current game
     pass
 
-def review_game():
+def review_game(): # Go through game move by move
     pass
